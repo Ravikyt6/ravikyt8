284cd5636ecc884ecad50c2a7c680f430630ed78
ranig8879@gmail.com <ranig8879@gmail.com>
2022-07-14 13:38:03 +0530

    p

---
+++ type.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typed", { 
+
+            strings: ["",
+
+            'youtuber','web developer','student'], 
+
+              
+
+            typeSpeed: 100, 
+
+            backSpeed:60, 
+
+            loop: true, 
+
+        });
